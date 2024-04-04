a66d606aba8fd8fbdf895f6ca7fe0cd60b3ddf64
dbejo <bejodavid123@gmail.com>
2024-04-04 14:47:52 +0200

    Use hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import Navbar from "./components/Navbar";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
 import PageWrapper from "./components/PageWrapper";
 import Footer from "./components/Footer";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Home />,
